2ffc4977bf733c6f7a1f1d49359eff0e8b62ebad
Connor Van Elswyk <ConnorVanElswyk@gmail.com>
2022-01-27 19:38:36 -0500

    better math for chips, also hiding empty roi

--- src/components/chips/Chip.vue
+++ src/components/chips/Chip.vue
@@ -1,5 +1,6 @@
 <template>
   <v-chip
+      v-show="valid"
       :class="clazz"
       :color="color"
       :small="small"
@@ -24,16 +25,20 @@ export default {
 
   computed: {
 
+    valid() {
+      return this.text !== ""
+    },
+
     disabled() {
       return this.text === "0%"
     },
 
     color() {
-      if (this.float === -100) {
+      if (this.float <= -100) {
         return 'pink accent-3'
       } else if (this.float < -35) {
         return 'orange accent-3'
-      } else if (this.float <= -1) {
+      } else if (this.float < 0) {
         return 'amber accent-3'
       } else if (this.float > 100) {
         return 'green accent-3'
