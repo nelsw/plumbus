8b88470607efd2d3ed5fe0cbf243c3908e069b3f
Connor Van Elswyk <ConnorVanElswyk@gmail.com>
2022-01-27 19:38:17 -0500

    add disabled flag to expand button

--- src/components/buttons/ExpandButton.vue
+++ src/components/buttons/ExpandButton.vue
@@ -1,7 +1,7 @@
 <template>
     <v-tooltip top>
       <template v-slot:activator="{ on, attrs }">
-        <v-btn icon :small="small" :color="color" v-on="on" v-bind="attrs" @click="expand(!isExpanded)">
+        <v-btn icon :small="small" :color="color" v-on="on" v-bind="attrs" @click="expand(!isExpanded)" :disabled="disabled">
           <v-icon :small="small" v-text="`mdi-chevron-${isExpanded ? 'up' : 'down'}`"/>
         </v-btn>
       </template>
@@ -17,6 +17,7 @@ export default {
     color: String,
     expand: Function,
     isExpanded: Boolean,
+    disabled: Boolean,
   },
 }
 </script>
